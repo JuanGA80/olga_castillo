1d56f25c48cedc916f34704380dc5f7c7badbaf6
Joan Kenedi Mayo <joankenedi9311@gmail.com>
2022-04-07 13:27:51 -0500

    ultimos_cambios

--- js/submenu.js
+++ js/submenu.js
@@ -1,4 +1,5 @@
 let listElements = document.querySelectorAll('.btn_despega');
+let list = document.getElementById('tratamientos');
 
 listElements.forEach( listElements =>{
    listElements.addEventListener('click', ()=>{
@@ -6,12 +7,16 @@ listElements.forEach( listElements =>{
 
        let height = 0;
        let menu = listElements.nextElementSibling;
+     
        if (menu.clientHeight == "0") {
-          
+        
            height = menu.scrollHeight;
+        
+        
        }
        
        menu.style.height = `${height}px`;
+    
    })
 
 
